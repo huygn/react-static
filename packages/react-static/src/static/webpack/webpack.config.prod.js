fef847b43dd672628dcc802d00f5cf04661bd932
Bram Loogman <info@bramloogman.nl>
2020-04-08 18:15:25 +0200

    Fix webpack.externals for node
    Based on: https://github.com/react-static/react-static/pull/1383
    
    This replaces webpack-node-externals with a resolve-from based externals config in webpack.config.prod.js that fixes the monorepo support in react-static.
    
    This fixes an issue where multiple react instance where found during the build which will throw an error when using hooks. See: https://github.com/react-static/react-static/issues/1216

--- packages/react-static/src/static/webpack/webpack.config.prod.js
+++ packages/react-static/src/static/webpack/webpack.config.prod.js
@@ -6,7 +6,7 @@ import TerserPlugin from 'terser-webpack-plugin'
 import nodeExternals from 'webpack-node-externals'
 import ExtractCssChunks from 'extract-css-chunks-webpack-plugin'
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
-import resolveFrom from 'resolve-from'
+import { silent as resolveFrom } from 'resolve-from'
 //
 import rules from './rules'
 
@@ -165,16 +165,31 @@ export default function(state) {
   result.devtool = false
   result.externals = [
     new RegExp(`${paths.PLUGINS}`),
+    // Externalise all node_modules except for react-static files that need to be bundled
     (context, request, callback) => {
-      const resolved = path.resolve(context, request)
-      if (
-        [/react-static(\\|\/)lib(\\|\/)browser/, /webpack-flush-chunks/].some(
-          d => d.test(resolved)
-        )
-      ) {
-        return callback(null, `commonjs ${resolved}`)
+      // All paths starting with `./` or `/` may be bundled as usual
+      if (/^[./\\]/.test(request)) {
+        return callback()
       }
-      callback()
+
+      // Attempt to resolve the requested module or file from `context` which points at
+      // the file's location where the dependency on `request` is located at.
+      const res = resolveFrom(`${context}/`, request)
+
+      if (
+        res &&
+        // If the request is in node_modules and a js file it should be externalised,
+        /node_modules[/\\].*\.js$/.test(res) &&
+        // unless it's a react-static dependency
+        !/node_modules[/\\]react-static/.test(res) &&
+        // react-universal-component must be bundled for static export to work
+        // https://github.com/react-static/react-static/issues/1446
+        !/node_modules[/\\]react-universal-component/.test(res)
+      ) {
+        callback(null, `commonjs ${request}`)
+      } else {
+        callback()
+      }
     },
     nodeExternals({
       whitelist: ['react-universal-component'],
